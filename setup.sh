a72e7478f03c3a3612195eb66100f03647c4f3de
Nadav <nadavsuliman@gmail.com>
2021-04-03 13:05:26 +0300

    Moved Vagrantfile and setup.sh to another branch
    vagrant branch now includes Vagrantfile and setup.sh for users who do not have ubuntu installed.

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-# The -e option would make our script exit with an error if any command 
-# fails while the -x option makes verbosely it output what it does
-
-# Updating the apt-get
-sudo apt-update
-
-# Installing build-essential package to build the program.
-sudo apt-get install build-essential
